ecbfcd99251efaec511a67d4d11024a04d30c891
Ptitlu42 <lucas.beyer@gmx.fr>
2025-05-23 22:01:12 +0200

    Ajouter une route de test pour Products API

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get("/", function () {
+    return view("welcome");
+});
+
+Route::get("/api/test", function () {
+    return response()->json([
+        "status" => "success",
+        "service" => "products-api",
+        "message" => "Le service Products API fonctionne correctement",
+        "data" => [
+            "products_count" => 24,
+            "categories_count" => 5,
+            "version" => "1.0.0"
+        ]
+    ]);
 });
