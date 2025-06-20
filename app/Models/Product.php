cd0ea7133e601d0764f8eb7fd40771730ef5811a
JustManeR <m.robleguire@gmail.com>
2025-06-20 10:10:04 +0200

    création des modèles et migrations

---
+++ app/Models/Product.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+    protected $fillable = [
+        'name', 'description', 'origin', 'price', 'stock', 'orders_id'
+    ];
+
+    protected static function boot()
+    {
+        parent::boot();
+        static::creating(function ($model) {
+            $model->id = (string) Str::uuid();
+        });
+    }
+}
